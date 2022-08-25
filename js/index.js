0882467da226997b1d9b616f6779ac48445f96a7
Manish Choudhary <58661988+manish-9@users.noreply.github.com>
2022-08-25 15:57:33 +0530

    updated index.js
    improved code logic and flow

--- js/index.js
+++ js/index.js
@@ -4,7 +4,35 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 var colorHistory = [];
 var userClickedPattern = [];
 
+var started = false;
 var level = 0;
+
+// flash items on load
+$(".gameStatus").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+setTimeout(function () {
+  $("button").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+}, 1000);
+
+//initial color and action for game...
+$(document).keypress(function () {
+  if (!started) {
+    nextColorSequence();
+    started = true;
+  }
+});
+
+$("button").click(function () {
+  var currentColor = $(this).attr("id");
+  userClickedPattern.push(currentColor);
+
+  $("#" + currentColor)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  playSound(currentColor);
+  animatePress(currentColor);
+  checkAnswer(userClickedPattern.length - 1);
+});
 
 //function to store previous click pattern and to decide new color...
 function nextColorSequence() {
@@ -25,43 +53,23 @@ function nextColorSequence() {
     $("#" + newColor)
       .fadeOut(100)
       .fadeIn(100);
-  }, 1500);
+  }, 1200);
 
-  console.log(colorHistory);
+  // console.log(colorHistory);
 }
 
 function checkAnswer(currentIndex) {
   if (colorHistory[currentIndex] === userClickedPattern[currentIndex]) {
-    console.log(
-      "colors at index [" +
-        currentIndex +
-        "] of both user input array and game random array Match."
-    );
+    // console.log("colors at index [" + currentIndex + "] of both user input array and game random array Match.");
 
     if (userClickedPattern.length === colorHistory.length) {
       setTimeout(function () {
         nextColorSequence();
-      }, 1200);
+      }, 900);
     }
   } else {
-    console.log(
-      "colors at index [" +
-        currentIndex +
-        "] of both user input array and game random array did NOT Match."
-    );
-    $(".gameStatus").text("Game Over, Press Any Key to Restart...");
-    playSound("wrong");
-    $("body").addClass("gameOver");
-    setTimeout(function () {
-      $("body").removeClass("gameOver");
-    }, 100);
-
-    $(document).keypress(function () {
-      colorHistory = [];
-      level = 0;
-      nextColorSequence();
-    });
-    // nextColorSequence();
+    // console.log("colors at index [" +currentIndex + "] of both user input array and game random array did NOT Match.");
+    startAgain();
   }
 }
 
@@ -73,33 +81,30 @@ function animatePress(currentColor) {
   }, 100);
 }
 
+// function to play sounds...
 function playSound(soundName) {
   var sound = new Audio("sounds/" + soundName + ".mp3");
   sound.play();
 }
 
-// flash items on load
-$(".gameStatus").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-setTimeout(function () {
-  $("button").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-}, 1000);
+// function to Restart game...
+function startAgain() {
+  
+  $(".gameStatus").html(
+    'Game Over! Click "<span class="btn gameStatus">Restart</span>" to Start Again'
+  );
 
-//initial color and action for game...
-$(document).keypress(function () {
-  if (colorHistory.length === 0) {
+  playSound("wrong");
+
+  $("body").addClass("gameOver");
+  setTimeout(function () {
+    $("body").removeClass("gameOver");
+  }, 100);
+
+  $("span.btn").click(function () {
+    colorHistory = [];
+    level = 0;
+    started = false;
     nextColorSequence();
-  }
-});
-
-$("button").click(function () {
-  var currentColor = $(this).attr("id");
-  userClickedPattern.push(currentColor);
-
-  $("#" + currentColor)
-    .fadeOut(100)
-    .fadeIn(100);
-
-  playSound(currentColor);
-  animatePress(currentColor);
-  checkAnswer(userClickedPattern.length - 1);
-});
+  });
+}
